393470b1433e464e90ffe6ffce09a1b63798b4e5
ha <hadyaamin712@gmail.com>
2026-06-14 13:00:59 +0430

    js added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "11887d043t216f31aoc82b0e0d11614d";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListner("submit", handleSearchSubmit);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("paris");
